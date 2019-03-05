6c4a40fc3afcdc74c8bd3fb3d50103d868ba6cdb
Roman Pinegin <roman.pinegin@gmail.com>
2019-03-05 21:00:35 +0200

    JavaScript

---
+++ Lesson4/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var num1, num2, result;
+
+num1 = +prompt('Введите 1ое число' , 0)
+num2 = +prompt('Введите 2ое число', 0)
+
+result = num1 + num2;
+
+alert('Результат сложения ' + result);
