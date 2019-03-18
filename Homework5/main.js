0444d90f48c1af1a5b5221b50cfbfb925662acc2
romanpinegin <roman.pinegin@gmail.com>
2019-03-18 22:57:44 +0200

    Шестое домашнее

---
+++ Homework5/main.js
@@ -0,0 +1,7 @@
+"use strict"
+
+var person = {};
+person.name = 'Вася';
+person.age = 25;
+alert(person.name + ' : ' + person.age);
+
---
+++ Homework6/main2.js
@@ -0,0 +1,4 @@
+"use strict";
+
+let diagonal = document.getElementById("diagonal1,diagonal2,diagonal3,diagonal4,diagonal5");
+diagonal.style.backgroundColor = 'red';
