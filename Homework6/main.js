14743b288c4cce94be8713bc2fcb07aa4d8e8a52
romanpinegin <roman.pinegin@gmail.com>
2019-03-18 23:18:34 +0200

    Шестое домашнее

--- Homework6/main.js
+++ Homework6/main.js
@@ -5,5 +5,3 @@ for (let i = 0; i < table.rows.length; i++) {
     let row = table.rows[i];
     row.cells[i].style.backgroundColor  = 'red';
 }
-
-
--- Homework6/main2.js
+++ Homework6/main2.js
@@ -1,4 +1,4 @@
 "use strict";
 
-let diagonal = document.getElementById("diagonal1,diagonal2,diagonal3,diagonal4,diagonal5");
+let diagonal = document.getElementById("");
 diagonal.style.backgroundColor = 'red';
